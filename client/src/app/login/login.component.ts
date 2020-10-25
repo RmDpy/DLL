305e2beeed811841f000d1c92a930e3f693ca501
RmDpy <remi.dupuysio@gmail.com>
2020-10-25 14:10:30 +0100

    mise en place service

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Enseignant} from "../../assets/classes/enseignant";
+import {UtilisateurService} from "../utilisateur.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  enseignants: Enseignant[] = [];
+
+  constructor(private utilisateurService: UtilisateurService) { }
+
+  ngOnInit(): void {
+    this.getEnseignants();
+  }
+
+  getEnseignants(): void {
+    this.utilisateurService.getAllEnseignant()
+      .subscribe()
+  }
+
+}
---
+++ client/src/app/utilisateur.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
+import { Enseignant} from "../assets/classes/enseignant";
+import {Etudiant} from "../assets/classes/etudiant";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilisateurService {
+  activedb: string = "mongo";
+  constructor(private http: HttpClient) { }
+
+  public setActivedb(db: string) {
+    this.activedb = db;
+  }
+
+  getAllEnseignant() {
+    return this.http.get(
+      environment.nodeApiUrl + "api/etudiants"
+    );
+  }
+}
